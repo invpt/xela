b19a28efd88ed97359a1f2b201c11951966e7e96
fixpt <57822954+fixpt@users.noreply.github.com>
2024-07-28 11:43:52 -0400

    Add KDF and use constants

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -5,9 +5,57 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"errors"
+	"fmt"
+
+	"golang.org/x/crypto/argon2"
 )
 
+// Length of the salt appended to every password
+const saltLength int = 32
+
+// Length of the encryption key
+const keyLength uint32 = 32
+
+// Length of the ciphertext of a superblock
+const cphtxtLen int = 256
+
+// Length of the interior of a superblock
+const intrLen int = cphtxtLen - aes.BlockSize
+
+// Length of the plaintext stored by a superblock
+const ptxtLen int = intrLen - 1
+
 // The Argon2 key derivation function is used to turn passwords into encryption keys.
+
+type Key struct{ key []byte }
+type Salt struct{ salt []byte }
+
+func GenerateSalt() (Salt, error) {
+	salt := make([]byte, saltLength)
+	_, err := rand.Read(salt)
+	if err != nil {
+		return Salt{}, err
+	}
+
+	return Salt{salt: salt}, nil
+}
+
+type KdfParameters struct {
+	time    uint32
+	memory  uint32
+	threads uint8
+}
+
+func DefaultKdfParameters() KdfParameters {
+	return KdfParameters{
+		memory:  64 * 1024,
+		threads: 4,
+	}
+}
+
+func DeriveKey(params KdfParameters, password []byte, salt Salt) Key {
+	return Key{key: argon2.IDKey(password, salt.salt, params.time, params.memory, params.threads, keyLength)}
+}
 
 // Each file is divided into superblocks that end up being 256 bytes large in the encrypted,
 // on-disk format. A superblock consists of an initialization vector (16 bytes) followed by 240
@@ -20,12 +68,8 @@ type XelaDecrypter struct {
 	cbc cipher.BlockMode
 }
 
-func NewXelaDecrypter(key []byte) (*XelaDecrypter, error) {
-	if len(key) != 32 {
-		return nil, errors.New("xela/crypto: incorrect size for the decryption key")
-	}
-
-	b, err := aes.NewCipher(key)
+func NewXelaDecrypter(key Key) (*XelaDecrypter, error) {
+	b, err := aes.NewCipher(key.key)
 	if err != nil {
 		return nil, err
 	}
@@ -35,26 +79,26 @@ func NewXelaDecrypter(key []byte) (*XelaDecrypter, error) {
 
 // Decrypts the given cyphertext and places it into the plaintext pointer.
 //
-// May reuse/write to any current byte array pointed to by plaintext.
+// May reuse/write to the current byte array pointed to by plaintext.
 func (x *XelaDecrypter) Decrypt(plaintext []byte, ciphertext []byte) ([]byte, error) {
-	if len(ciphertext)%256 != 0 {
-		return nil, errors.New("xela/crypto: malformed ciphertext - the length must be a multiple of 256")
+	if len(ciphertext)%cphtxtLen != 0 {
+		return nil, errors.New(fmt.Sprint("xela/crypto: malformed ciphertext - the length must be a multiple of ", cphtxtLen))
 	}
 
-	blockCount := len(ciphertext) / 256
+	blockCount := len(ciphertext) / cphtxtLen
 
 	plaintext = plaintext[:0]
 	for blockIndex := 0; blockIndex < blockCount; blockIndex++ {
-		if cap(plaintext)-len(plaintext) < 239 {
+		if cap(plaintext)-len(plaintext) < ptxtLen {
 			// grow the plaintext slice whenever needed
-			newPlaintext := make([]byte, len(plaintext)+239, max(len(plaintext)*2, len(plaintext)+239))
+			newPlaintext := make([]byte, len(plaintext)+ptxtLen, max(len(plaintext)*2, len(plaintext)+ptxtLen))
 			copy(newPlaintext[:len(plaintext)], plaintext[:])
 			plaintext = newPlaintext
 		}
 
 		superblockLength, err := x.DecryptSuperblock(
-			plaintext[len(plaintext):len(plaintext)+239],
-			ciphertext[blockIndex*256:(blockIndex+1)*256],
+			plaintext[len(plaintext):len(plaintext)+ptxtLen],
+			ciphertext[blockIndex*cphtxtLen:(blockIndex+1)*cphtxtLen],
 		)
 		if err != nil {
 			return nil, err
@@ -70,20 +114,20 @@ func (x *XelaDecrypter) Decrypt(plaintext []byte, ciphertext []byte) ([]byte, er
 //
 // All superblocks are decrypted independently.
 func (x *XelaDecrypter) DecryptSuperblock(plaintext, ciphertext []byte) (length int, err error) {
-	if len(ciphertext) != 256 || len(plaintext) != 239 {
+	if len(ciphertext) != cphtxtLen || len(plaintext) != ptxtLen {
 		return 0, errors.New("xela/crypto: incorrect size for plaintext or ciphertext parameter")
 	}
 
-	iv := ciphertext[:16]
+	iv := ciphertext[:aes.BlockSize]
 
 	x.cbc, err = setCBCIV(x.b, x.cbc, iv)
 	if err != nil {
 		return
 	}
 
-	plaintextWithLength := [240]byte{}
-	x.cbc.CryptBlocks(plaintextWithLength[:], ciphertext[16:])
-	length = int(plaintextWithLength[239])
+	plaintextWithLength := [intrLen]byte{}
+	x.cbc.CryptBlocks(plaintextWithLength[:], ciphertext[aes.BlockSize:])
+	length = int(plaintextWithLength[intrLen-1])
 	copy(plaintext[:length], plaintextWithLength[:length])
 
 	return
@@ -94,12 +138,8 @@ type XelaEncrypter struct {
 	cbc cipher.BlockMode
 }
 
-func NewXelaEncrypter(key []byte) (*XelaEncrypter, error) {
-	if len(key) != 32 {
-		return nil, errors.New("xela/crypto: incorrect size for decryption key")
-	}
-
-	b, err := aes.NewCipher(key)
+func NewXelaEncrypter(key Key) (*XelaEncrypter, error) {
+	b, err := aes.NewCipher(key.key)
 	if err != nil {
 		return nil, err
 	}
@@ -112,16 +152,16 @@ func NewXelaEncrypter(key []byte) (*XelaEncrypter, error) {
 // slice may also be written to.
 func (x *XelaEncrypter) Encrypt(ciphertext []byte, plaintext []byte) ([]byte, error) {
 	// TODO: optimize the diff for simplicity
-	blocksNeeded := len(plaintext) / 239
-	if len(plaintext)%239 != 0 {
+	blocksNeeded := len(plaintext) / ptxtLen
+	if len(plaintext)%ptxtLen != 0 {
 		blocksNeeded += 1
 	}
-	ciphertext = make([]byte, 0, blocksNeeded*256)
+	ciphertext = make([]byte, 0, blocksNeeded*cphtxtLen)
 
 	for blockIndex := 0; blockIndex < blocksNeeded; blockIndex++ {
 		err := x.EncryptSuperblock(
-			ciphertext[blockIndex*256:(blockIndex+1)*256],
-			plaintext[blockIndex*239:min((blockIndex+1)*239, len(plaintext))],
+			ciphertext[blockIndex*cphtxtLen:(blockIndex+1)*cphtxtLen],
+			plaintext[blockIndex*ptxtLen:min((blockIndex+1)*ptxtLen, len(plaintext))],
 		)
 		if err != nil {
 			return nil, err
@@ -132,11 +172,11 @@ func (x *XelaEncrypter) Encrypt(ciphertext []byte, plaintext []byte) ([]byte, er
 }
 
 func (x *XelaEncrypter) EncryptSuperblock(ciphertext, plaintext []byte) (err error) {
-	if len(ciphertext) != 256 {
+	if len(ciphertext) != cphtxtLen {
 		return errors.New("xela/crypto: incorrect size for ciphertext parameter")
 	}
 
-	iv := ciphertext[:16]
+	iv := ciphertext[:aes.BlockSize]
 	_, err = rand.Read(iv)
 	if err != nil {
 		return err
@@ -147,10 +187,10 @@ func (x *XelaEncrypter) EncryptSuperblock(ciphertext, plaintext []byte) (err err
 		return err
 	}
 
-	plaintextWithLength := [240]byte{}
+	plaintextWithLength := [intrLen]byte{}
 	copy(plaintextWithLength[:len(plaintext)], plaintext)
-	plaintextWithLength[239] = byte(len(plaintext))
-	x.cbc.CryptBlocks(ciphertext[16:], plaintextWithLength[:])
+	plaintextWithLength[intrLen-1] = byte(len(plaintext))
+	x.cbc.CryptBlocks(ciphertext[aes.BlockSize:], plaintextWithLength[:])
 
 	return
 }
