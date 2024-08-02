1f7a9eb32d8304e957980afedbdf7e3a1da8a80b
fixpt <57822954+fixpt@users.noreply.github.com>
2024-08-01 20:18:52 -0400

    Use go-git instead of git2go

--- sync/gitsync/gitsync.go
+++ sync/gitsync/gitsync.go
@@ -2,14 +2,17 @@ package gitsync
 
 import (
 	"fixpt.org/xela/vault/fsvault"
-	git "github.com/libgit2/git2go/v34"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport"
 )
 
-func Clone(from string, into string) (*fsvault.FSVault, error) {
-	_, err := git.Clone("", into, &git.CloneOptions{})
+func Clone(from string, into string, auth transport.AuthMethod) (*fsvault.FSVault, error) {
+	_, err := git.PlainClone(into, false, &git.CloneOptions{
+		URL:  from,
+		Auth: auth,
+	})
 	if err != nil {
 		return nil, err
 	}
-
 	return fsvault.Open(into), nil
 }
